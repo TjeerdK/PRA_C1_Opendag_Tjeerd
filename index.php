15258880b0df5a6dbdfb38809cda8ed95542a760
Tjeerd Kootwijk <tjeerdgaming@gmail.com>
2022-09-14 17:31:00 +0200

    start website
    index.html and css added

---
+++ index.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+  <meta charset="utf-8">
+  <title>HomePage</title>
+
+  <link rel="stylesheet" href="css/styles.css">
+
+</head>
+
+<body>
+    
+<div class="homepage">
+    <div class="nav">
+        <div class="logo">
+            <img src="img/curio-02-wit-logo-rgb.png" alt="Curio_Logo">
+        </div>
+        <div class="ankers">
+            <a href="#Tjeerd">Tjeerd Kootwijk</a>
+            <a href="#SSD2E">SSD2E</a>
+        </div>
+    </div>
+    <div class="banner">
+        <div class="bannerInfo">
+            <div class="infoFiler">
+            </div>
+            <div class="infoAnkers">
+                <a href="#Vakken">Vakken</a>
+                <a href="#Rooster">Rooster</a>
+                <a href="#Blokken">Blokken</a>
+                <a href="#Versnellen">Versnellen</a>
+                <a href="#Docenten">Docenten</a>
+                <!-- <h1><a name="Vakken"></a>TOP</h1>
+                <h1><a name="Rooster"></a>TOP</h1>
+                <h1><a name="Blokken"></a>TOP</h1>
+                <h1><a name="Versnellen"></a>TOP</h1> -->
+            </div>
+        </div>
+    </div>
+    <section class="Information">
+        <div class="Vakken">
+            <h1><a  name="Vakken"></a> Vakken</h1>
+            <h3>Nederlands</h3>
+            <h3>Engels</h3>
+            <h3>Rekenen</h3>
+            <h3>Webdevelopment</h3>
+            <h3>Native</h3>
+            <h3>Prijktijk</h3>
+            <h3>Prijktijk ondersteuning</h3>
+        </div>
+
+        <div class="Rooster">
+            <h1><a name="Rooster"></a> Rooster</h1>
+            <p></p>
+    </section>
+</div>
+
+</body>
+
+</html>
