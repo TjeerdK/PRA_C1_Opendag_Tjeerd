09e3a2197e853c8e98b93b8b8383d79093d566cf
Tjeerd Kootwijk <tjeerdgaming@gmail.com>
2022-10-01 16:16:31 +0200

    added arcordion data
    -docenten carocel
    - vaken text
    -rooster text
    -kosten text
    -bloken text
    -versnellen text

--- index.php
+++ index.php
@@ -7,6 +7,10 @@
 
   <link rel="stylesheet" href="css/styles.css">
 
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 </head>
 
 <body>
@@ -26,11 +30,12 @@
             <div class="infoFiler">
             </div>
             <div class="infoAnkers">
-                <a href="#Vakken">Vakken</a>
-                <a href="#Rooster">Rooster</a>
-                <a href="#Blokken">Blokken</a>
-                <a href="#Versnellen">Versnellen</a>
-                <a href="#Docenten">Docenten</a>
+                
+                <a href="#Vakken" class="NavItem" id="Test">Vakken</a>
+                <a href="#Rooster" class="NavItem" id="Test">Rooster</a>
+                <a href="#Blokken" class="NavItem" id="Test">Blokken</a>
+                <a href="#Versnellen" class="NavItem" id="Test">Versnellen</a>
+                <a href="#Docenten" class="NavItem" id="Test">Docenten</a>
                 <!-- <h1><a name="Vakken"></a>TOP</h1>
                 <h1><a name="Rooster"></a>TOP</h1>
                 <h1><a name="Blokken"></a>TOP</h1>
@@ -40,23 +45,79 @@
     </div>
     <section class="Information">
         <div class="Vakken">
-            <h1><a  name="Vakken"></a> Vakken</h1>
-            <h3>Nederlands</h3>
-            <h3>Engels</h3>
-            <h3>Rekenen</h3>
-            <h3>Webdevelopment</h3>
-            <h3>Native</h3>
-            <h3>Prijktijk</h3>
-            <h3>Prijktijk ondersteuning</h3>
+            <button class="accordion"><a name="Vakken"></a> Vakken</button>
+            <div class="panel">
+            <?php
+                $Vakken = ['Nederlands', 'Engels', 'Rekenen', 'Webdevelopment', 'Native', 'Praktijk', 'Praktijk ondersteuning'];
+                foreach ($Vakken as $Vak) {
+                    //list-group-item
+                    echo "<p >$Vak</p>";
+                }
+            ?>   
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel numquam assumenda, provident repellendus sit, ipsam impedit tempore, atque modi ab in cum consequatur! Vero dolorem quidem eum dolorum fuga corrupti perferendis, dolores labore neque sit alias, tempora iusto impedit recusandae nesciunt. Pariatur magnam aliquid veritatis animi dolorem nobis neque! Odio?</p>
+            </div>         
         </div>
 
         <div class="Rooster">
-            <h1><a name="Rooster"></a>Rooster</h1>
-            <p></p>
+            <button class="accordion"><a name="Rooster"></a> Rooster</button>
+            <div class="panel">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae ut autem vel minus odio, facere debitis ab blanditiis nulla sapiente mollitia expedita optio omnis impedit perferendis animi reiciendis labore fuga eum voluptatibus deleniti aperiam obcaecati pariatur fugit? Et nisi ducimus, autem odit iusto deserunt tempora quas inventore quibusdam distinctio illo fugiat nesciunt labore tempore? Quos iure dolore, necessitatibus unde in dicta est voluptate quia fuga alias voluptates dolor optio vitae? Modi iure harum commodi? Ullam repellat quam, quaerat accusantium adipisci ratione maxime. Dolorum magni, sequi nam doloribus tenetur doloremque cupiditate quam ullam, adipisci veritatis, consequuntur beatae asperiores porro unde omnis!</p>
+            </div>
         </div>
+
+        <div class="Blokken">
+            <button class="accordion"><a name="Blokken"></a> Blokken</button>
+            <div class="panel">
+            <p>Bij ons op de opleiding software developer hebben wij een blokken systeem. Elk blok is een half jaar waar je de stof in leert. De opleiding heeft dus acht blokken in totaal, maar als je de opleiding versnelt doet heb je maar zes blokken omdat de opleiding dan maar 3 jaar duurt. Het is heel fijn om met dit systeem te werken want als je bijvoorbeeld een keer de doelen niet haalt die je af moest hebben en onder de knie hoef je niet het hele jaar overnieuw te doen maar hoef je alleen dat blok die je niet hebt gehaald overnieuw doen. Dat scheelt heel veel tijd en is het makkelijker om weer verder te gaan met waar je eigenlijk zou moeten zijn. Elk blok bestaat uit 16 weken. Aan het einde van de 16 weken krijg je meestal een toets om te kijken of je alles onder de knie hebt en alles hebt begrepen. Ook heb je dan twee weken om alle opdrachten die je nog niet af had of nog opnieuw moet doen in te leveren zodat je het blok misschien nog net haalt. In die twee weken worden meestal ook de toetsen van Web development en Native gehouden. Dat zijn een van de twee belangrijkste vakken dus hebben ze daar voor een grootte toets. Voor de andere vakken krijg je een toets die je in de les moet maken.</p>
+            </div>
+
+        </div>
+        <div class="versnellen">
+            <button class="accordion"><a name="Versnellen"></a> Versnellen</button>
+            <div class="panel">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam, rerum omnis, consequatur sed nihil eum corrupti nesciunt sit iure dicta voluptate eligendi fugit velit quo, mollitia ducimus. Exercitationem totam fuga inventore numquam? Blanditiis, tenetur quae! Quod iure dolores enim voluptates eveniet nihil dolor veritatis delectus vero blanditiis natus voluptatem, soluta eius vel sunt ullam laborum perferendis quis deserunt magni quidem debitis? Hic rerum neque odio? Quis corrupti cumque deleniti odit illum dolore ducimus dicta placeat ut nostrum voluptatem incidunt saepe, temporibus perferendis! Amet ullam nostrum, ea itaque nihil voluptates minima consectetur est commodi, voluptate dolores ab aspernatur harum aperiam vero.</p>
+            </div>
+        </div>
+        <div class="Docenten">
+            <button class="accordion"><a name="Docenten"></a> Docenten</button>
+            
+                <div class="panel">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet eum, doloribus dolorum vitae exercitationem debitis eaque fugit ex. Ea quis dolor rem voluptate sapiente temporibus tempore repellat nihil. Fuga possimus, dolorem minima amet hic perspiciatis fugiat qui? Debitis, illo veritatis. Asperiores cupiditate doloremque sit, incidunt beatae et voluptatibus corrupti enim.</p>
+
+                    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active" >
+                                <img class="d-block docenten_img" src="img/docenten/corne.png" alt="Second slide">
+                            </div>
+                                <?php
+                                $files = glob('img/docenten/*.{jpg,png,gif}', GLOB_BRACE);
+                                foreach($files as $file) {
+                                echo "<div class='carousel-item '>";
+                                echo  "<img class='d-block docenten_img' src='$file' alt='First slide'>";
+                                echo "</div>";
+                                }
+                                ?>   
+                        </div> 
+                        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                </div>
+        </div>
+        <div class="Kosten">
+            <button class="accordion"><a name="Kosten"></a> Kosten</button>
+            <div class="panel">
+            <p>Voor deze opleiding heb je niet al te veel nodig maar een aantal dingen zijn wel cruciaal om te hebben. Zoals een laptop met specifieke eisen die er in zitten. Denk daarbij aan ram geheugen, hoeveelheid opslag en CPU. Ook heb je voor sommige vakken zoals Nederlands Engels en rekenen licenties nodig om de online programma’s te gebruiken om die vakken te beoefenen. Het is ook handig om bepaalde programma’s alvast te downloaden. Die programma’s zijn helemaal gratis en heb je nodig om in te werken. Visual studio is een programma waar je in gaat werken in blok C om c# code te schrijven. Ook heb je Visual studio code dat is een programma van de zelfde makers en dat kan je al gebruiken in het eerste blok voor je websites die je gaat maken. Ook ga je python schrijven in Visual studio code. Je begint het jaar door een micro bit te programmeren. Die moet je ook aanschaffen.</p>
+            </div>
+
     </section>
 </div>
-
+<script src="js/scripts.js"></script>
 </body>
 
 </html>
